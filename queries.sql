c698e5b73327c76e239131f1e4f4917af989a8ea
Bag0niku <45112126+Bag0niku@users.noreply.github.com>
2022-07-26 15:03:37 -0700

    Current employees that are expected to retire, previous list did not include this restriction

--- queries.sql
+++ queries.sql
@@ -1,7 +1,12 @@
--- Employees expected to start retiring are born in 1952 to 1955, 
--- and started working with the company in 1985 to 1988.
-SELECT *
+-- Current Employees that are expected to start retiring are born in 1952 to 1955, 
+-- and started working with the company in 1985 to 1988, including their department and position.
+SELECT e.emp_no, e.first_name, e.last_name, d.dept_name, ti.title
 INTO retirement_info
-FROM employees
-WHERE (birthdate BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+FROM dept_emp as de
+INNER JOIN employees AS e ON de.emp_no = e.emp_no
+INNER JOIN departments AS d ON de.dept_no = d.dept_no
+INNER JOIN titles AS ti ON ti.emp_no = de.emp_no
+WHERE (de.to_date = ('9999-01-01'))
+AND (ti.to_date = '9999-01-01')
+AND (e.birthdate BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
