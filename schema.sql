5b93956ed63a9cf66f1cc58792139b6556793c4c
Bag0niku <45112126+Bag0niku@users.noreply.github.com>
2022-07-25 18:16:39 -0700

    building the schema in sql, some tables missing primary keys

---
+++ schema.sql
@@ -0,0 +1,51 @@
+-- Creating the tables for PH-EmployeeDB
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL UNIQUE,
+	dept_name VARCHAR(40) NOT NULL UNIQUE,
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL UNIQUE,
+	birthdate DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR(4),
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(40),
+	from_date DATE,
+	to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE  dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE  salaries (
+	emp_no INT NOT NULL,
+	salary BIGINT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE  dept_managers (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	From_date DATE NOT NULL,
+	to_date DATE,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
