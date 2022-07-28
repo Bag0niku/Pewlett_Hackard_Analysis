e6abac2d3bb76369183ea48084d553e7b01d58d9
Bag0niku <45112126+Bag0niku@users.noreply.github.com>
2022-07-27 18:30:00 -0700

    the queries for the last tables uploaded

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,28 @@
+-- Job titles for potential retiries
+SELECT e.emp_no, e.first_name, e.last_name, titles.title, titles.from_date, titles.to_date
+INTO retirement_titles
+FROM employees as e
+JOIN titles ON e.emp_no = titles.emp_no
+WHERE e.birthdate BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no;
+
+
+-- retirement_titles has duplicate entries for employees due to promotions and job changes. 
+-- make them unique and only current employees, 
+-- exported all columns except to_date because it was not needed for report, but is needed to filter and create the list
+SELECT DISTINCT (emp_no), first_name, last_name, title, to_date
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date= '9999-01-01')
+ORDER BY to_date DESC, emp_no;
+
+
+-- count of titles about to retire
+SELECT COUNT (emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+
+
